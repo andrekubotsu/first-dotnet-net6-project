6f791395a346cbdd3aed045a73303a8452483f30
Andre Kubotsu <contato201@gmail.com>
2022-02-07 08:56:26 -0300

    pilha project added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using EstruturaDoPrograma;
+
+namespace EstruturaDoPrograma
+{
+    class Program
+    {
+        static void Main()
+        {
+            var salto = new Pilha();
+
+            salto.Empilha(0);
+            salto.Empilha(1);
+            salto.Empilha(10);
+            salto.Empilha(100);
+            salto.Empilha(1000);
+            salto.Empilha(10000);
+            salto.Empilha(100000);
+            salto.Empilha(1000000);
+
+            Console.WriteLine(salto.Desempilha());
+            Console.WriteLine(salto.Desempilha());
+            Console.WriteLine(salto.Desempilha());
+            Console.WriteLine(salto.Desempilha());
+            Console.WriteLine(salto.Desempilha());
+            Console.WriteLine(salto.Desempilha());
+            Console.WriteLine(salto.Desempilha());
+            Console.WriteLine(salto.Desempilha());
+            Console.WriteLine("Desempilhamento Realizado com Sucesso!!!");
+
+        }
+    }
+}
--- README.md
+++
@@ -1,9 +0,0 @@
-# My first .NET project
-
-This is my first .NET project. I'm using it to learn C# and .NET6. I am actually a front-end developer, but I would like to understand better how .NET works, mostly of my job these days are consuming REST APIs written with .NET. Plus, who knows? It is always interesting learn! I am using VSCode + extensions.
-
-The ideia is to evolve this repo saving each step in a different branch.
-
-## Dependencies
-
-- .NET6 SDK
